12c0f3a48d910887a5150a4e81b358318eb6dd64
John Chaffee <johnchaffee@mac.com>
2021-06-30 16:11:00 -0700

    Add generic webhook auto-responder

--- src/create-app.js
+++ src/create-app.js
@@ -1,22 +1,40 @@
-const express = require('express');
-const { v1: uuidv1 } = require('uuid');
+const express = require("express");
+const { v1: uuidv1 } = require("uuid");
 
 module.exports = (config) => {
-    const app = express();
+  const app = express();
 
-    const requestFilter = (req, res, next) => {
-        res.locals.log = logWithRequestData(req.method, req.path, uuidv1());
-        next();
+  // *** BEGIN ZIPWHIP AND TWILIO WEBHOOK RESPONDER *** //
+  // ZIPWHIP WEBHOOK
+  // Reply with status 200
+  app.post(/\/(receive|progress|send|stop)/, (req, res, next) => {
+    console.log("ZIPWHIP WEBHOOK");
+    res.sendStatus(200);
+  });
+
+  // TWILIO WEBHOOK
+  // Reply with empty TWIML response
+  app.post(/\/twilio/, (req, res, next) => {
+    console.log("TWILIO WEBHOOK");
+    res.send("<Response></Response>");
+  });
+  // *** END ZIPWHIP AND TWILIO WEBHOOK RESPONDER *** //
+
+  const requestFilter = (req, res, next) => {
+    res.locals.log = logWithRequestData(req.method, req.path, uuidv1());
+    next();
+  };
+
+  const logWithRequestData =
+    (method, path, id) =>
+    (...message) => {
+      console.log(`[${method}][${path}][${id}]`, ...message);
     };
 
-    const logWithRequestData = (method, path, id) => (...message) => {
-        console.log(`[${method}][${path}][${id}]`, ...message);
-    };
+  app.enable("trust proxy"); // for trusting heroku proxy
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(requestFilter);
 
-    app.enable('trust proxy'); // for trusting heroku proxy
-    app.use(express.json());
-    app.use(express.urlencoded());
-    app.use(requestFilter);
-
-    return app
+  return app;
 };
