8c6d0291d0a45893f4a57e359ccf11990ee07d64
John Chaffee <johnchaffee@mac.com>
2021-07-07 11:49:14 -0700

    Add profile flag to twilio token:chat command

--- scripts/add-participants.sh
+++ scripts/add-participants.sh
@@ -20,7 +20,7 @@ echo CHAT_SERVICE_ID $CHAT_SERVICE_ID
 # twilio api:conversations:v1:conversations:participants:create --conversation-sid="$CONVERSATION_SID" --messaging-binding.address "$MOBILE_NUMBER" --messaging-binding.proxy-address "$TWILIO_LANDLINE"
 
 # # Create long-lived (24 hours in seconds) token for web agent
-# TOKEN=`twilio token:chat --chat-service-sid "$CHAT_SERVICE_ID" --identity "Web Agent" --ttl 86400`
+# TOKEN=`twilio token:chat --chat-service-sid "$CHAT_SERVICE_ID" --identity "Web Agent" --ttl 86400 --profile jc`
 # echo TOKEN: $TOKEN
 
 # Add Web Agent participant
